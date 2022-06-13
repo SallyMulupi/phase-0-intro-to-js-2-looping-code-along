d1a5d2d855e65421e79b48bdc8f98373ce26eb74
SallyMulupi <mulupisally2gmail.com>
2022-06-13 16:58:40 +0300

    done

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 // Code your solutions in this file
-const writeCards = (names) => {
-    return names.map(name => `Thank you, ${name}, for the wonderful surprise gift!`);
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
 }
 
-const countDown = (number) => {
-    for (let i = number; i >= 0; i--) {
-        console.log(i)
+wrapGifts(gifts);
+const string = ["Sally", "Keith", "Eugene"];
+const action = 'suprise';
+
+function writeCards(string, action) {
+
+    let peopleNames = [];
+    for (let i = 0; i < string.length; i++) {
+        peopleNames.push(`Thank you so much guys, ${string[i]}, for the lovely and adorable  ${action} gift!`);
+    }
+    return peopleNames;
+    debugger;
+}
+console.log(writeCards(string));
+
+function countDown(x) {
+    while (x > -1) {
+        console.log(x)
+        x--;
     }
 }
